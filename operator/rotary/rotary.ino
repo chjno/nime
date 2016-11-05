8cd0f8e167b5da2046c2a829b2dd9f11829ee848
Chino Kim <chjno.k@gmail.com>
2016-11-05 16:01:38 -0400

    rotary dialing + hook + lcd

--- operator/rotary/rotary.ino
+++ operator/rotary/rotary.ino
@@ -1,41 +1,6 @@
-//#include <LiquidCrystal.h>
-//
-//LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
-const int in = 8;
 const int tickThresh = 120;
-bool tick = false;
-int ticks = 0;
-long lastTickTime = 0;
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(in, INPUT);
-
-//  lcd.begin(16, 2);
-}
-
-void loop() {
-  if (millis() - lastTickTime > tickThresh && ticks > 0){
-    Serial.println(ticks);
-//    lcd.print(ticks);
-    ticks = 0;
-  }
-  
-  if (digitalRead(in) == LOW){
-    if (!tick){
-      Serial.println("low");
-      ticks++;
-      tick = true;
-      lastTickTime = millis();
-//      Serial.println(lastTickTime);
-    }
-  } else {
-    if (tick){
-      Serial.println("high");
-      tick = false; 
-    }
-  }
+#include <LiquidCrystal.h>
 
 //  // set the cursor to column 0, line 1
 //  // (note: line 1 is the second row, since counting begins with 0):
@@ -43,4 +8,65 @@ void loop() {
 //  // print the number of seconds since reset:
 //  lcd.print(millis() / 1000);
 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const int dialPin = 8;
+const int hookPin = 9;
+
+bool tick = false;
+int ticks = 0;
+long lastTickTime = 0;
+bool onHook = false;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(dialPin, INPUT);
+
+  lcd.begin(16, 2);
 }
+
+void loop() {
+  
+  hook();
+  if (!onHook){
+    dial();
+  }
+
+}
+
+void dial(){
+  if (millis() - lastTickTime > tickThresh && ticks > 0){
+    Serial.print(ticks);
+    lcd.print(ticks);
+    ticks = 0;
+  }
+  
+  if (digitalRead(dialPin) == LOW){
+    if (!tick){
+//      Serial.println("low");
+      ticks++;
+      tick = true;
+      lastTickTime = millis();
+//      Serial.println(lastTickTime);
+    }
+  } else {
+    if (tick){
+//      Serial.println("high");
+      tick = false; 
+    }
+  }
+}
+
+bool hook(){
+  if (digitalRead(hookPin) == LOW){
+    if (onHook){
+      onHook = false;
+    }
+  } else {
+    if (!onHook){
+      Serial.print('.');
+      lcd.clear();
+      onHook = true;
+    }
+  }
+}
+
