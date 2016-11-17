35afabadf1b49f37b9e972e7f381ad8fd8977525
Chino Kim <chjno.k@gmail.com>
2016-11-16 22:21:38 -0500

    untested: mega effects data

--- operator/ino_operator/mega_rcs/mega_rcs.ino
+++ operator/ino_operator/mega_rcs/mega_rcs.ino
@@ -1,5 +1,6 @@
 const int outputPins[10] = {A0,A1,A2,A3,A4,A5,A6,A7,A8,A9};
 const int inputPins[50] = {2,3,4,5,6,7,8,9,10,11,12,A13,14,15,16,17,18,19,A14,A15,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51};
+int sampleState[10] = {31,32,33,34,35,36,37,38,39,40};
 
 const byte outputBytes[10] = {
   B00000001,
@@ -39,7 +40,7 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0; i < 10; i++){
+  for (int i = 0; i < sizeof(outputPins); i++){
     if (i < 8){
       PORTK = B00000000;
       PORTF = outputBytes[i];
@@ -48,12 +49,32 @@ void loop() {
       PORTK = outputBytes[i];
     }
 
-    for (int j = 0; j < 50; j++){
+    for (int j = 0; j < sizeof(inputPins); j++){
 //      if ((j != 11) && (j != 18) && (j != 19)){
         if (digitalRead(inputPins[j]) == HIGH){
-          Serial.println(String(j + 2));
+          if (sampleState[i] != j){
+//          if (sampleState[i] == 0){ // if not using normal plugs
+            Serial.print(String(i + 1));
+            Serial.print(' ');
+            Serial.println(String(j + 2));
+            sampleState[i] = j;
+//            sampleState[i] = 1; // if not using normal plugs
+          }
+          
+        } else {
+          if (sampleState[i] != i + 31){
+//          if (sampleState[i] == 1){ // if not using normal plugs
+            Serial.print(String(i + 1));
+            Serial.print(' ');
+            Serial.println(String(i + 31)); // if not using normal plugs
+            sampleState[i] = i + 31;
+//            Serial.println('0');            // if using normal plugs
+//            sampleState[i] = 0;
+          }
+          
         }
 //      }
     }
   }
 }
+
