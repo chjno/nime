b66b8c77411f0d935d75ad1471cfa8f83d3fb048
Chino Kim <chjno.k@gmail.com>
2016-12-04 19:07:56 -0500

    revamp-dialing,hook&lcd working

--- operator/ino_operator/dialing/rotary_lcd_pcb/rotary_lcd_pcb.ino
+++ operator/ino_operator/dialing/rotary_lcd_pcb/rotary_lcd_pcb.ino
@@ -1,76 +1,101 @@
-const int tickThresh = 120;
-
 #include <LiquidCrystal.h>
 
-//  // set the cursor to column 0, line 1
-//  // (note: line 1 is the second row, since counting begins with 0):
-//  lcd.setCursor(0, 1);
-//  // print the number of seconds since reset:
-//  lcd.print(millis() / 1000);
-
 LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
-const int dialPin = 8;
-const int hookPin = 9;
+const int _rotaryPin = A0;
+const int _hookPin = A2;
 
-bool tick = false;
-int ticks = 0;
-long lastTickTime = 0;
-bool onHook = false;
+// lcd vars
+int _lcdCounter = 0;
+
+// on vars
+bool _plugged = false;
+
+// dial vars
+const int _tickChannel = 120;
+const int _tickThresh = 120;
+
+bool _ticked;
+int _ticks = 0;
+long _lastTickTime = 0;
+
+
 
 void setup() {
   Serial.begin(9600);
-  pinMode(dialPin, INPUT);
+  pinMode(_rotaryPin, INPUT);
 
   lcd.begin(16, 2);
 }
 
 void loop() {
-  
-  hook();
-  if (!onHook){
-    if (Serial.available()){
-      Serial.read();
-      lcd.clear();
-    }
-    dial();
-  }
+  if (on()){
+    int num = dial();
+    if (num > 0){
 
-}
-
-void dial(){
-  if (millis() - lastTickTime > tickThresh && ticks > 0){
-    Serial.print(ticks);
-    lcd.print(ticks);
-    ticks = 0;
-  }
-  
-  if (digitalRead(dialPin) == LOW){
-    if (!tick){
-//      Serial.println("low");
-      ticks++;
-      tick = true;
-      lastTickTime = millis();
-//      Serial.println(lastTickTime);
+      // send to twilio
+      Serial.println(num);
+      lcdDisplay(num); 
     }
   } else {
-    if (tick){
-//      Serial.println("high");
-      tick = false; 
-    }
+    lcd.clear();
   }
 }
 
-void hook(){
-  if (digitalRead(hookPin) == LOW){
-    if (onHook){
-      onHook = false;
+void lcdDisplay(int number){
+  if (_lcdCounter == 3 || _lcdCounter == 6){
+        lcd.print('-');
+      } else if (_lcdCounter == 10){
+        lcd.setCursor(0, 1);
+      } else if (_lcdCounter > 25){
+        lcd.setCursor(0, 1);
+        lcd.print("                ");
+        lcd.setCursor(0, 1);
+        _lcdCounter = 10;
+      }
+      lcd.print(number);
+      _lcdCounter++;
+}
+
+bool on(){
+  if (digitalRead(_hookPin) == LOW){
+    if (_plugged){
+      _plugged = false;
+      return false;
     }
   } else {
-    if (!onHook){
-      Serial.print('.');
-      lcd.clear();
-      onHook = true;
+    if (!_plugged){
+      _plugged = true;
+      return true;  
     }
+  }
+}
+
+int dial(){
+  if (digitalRead(_rotaryPin) == LOW){
+    if (!_ticked){
+      _ticked = true;
+      _lastTickTime = millis();
+    }
+  } else {
+    if (_ticked){
+      _ticked = false;
+      if (millis() - _lastTickTime > 10){
+//        troubleshoot ticks
+//        Serial.println(millis() - lastTickTime);
+        _ticks++;
+
+        // send to max
+        Serial.println(_tickChannel);
+      }
+    }
+  }
+
+  if (millis() - _lastTickTime > _tickThresh && _ticks > 0){
+    int ticks = _ticks;
+    _ticks = 0;
+    return ticks;
+  } else {
+    return 0;
   }
 }
 
